34e334bba535a3de8d138532ac0d1873599c2061
HedayaAlmomani <almomani.hedaya@gmail.com>
2022-06-01 14:07:35 +0300

    createRole

---
+++ backend/routes/role.js
@@ -0,0 +1,5 @@
+const express = require("express")
+const roleRouter = express.Router();
+const createRole = require("../controllers/role")
+roleRouter.post("/" , createRole)
+module.exports = roleRouter

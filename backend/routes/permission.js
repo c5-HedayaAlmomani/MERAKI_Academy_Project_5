cabf39419b0a4e1c5088ac8880785c27c1fa192c
jehadalhorany <alhoranyjehad@gmail.com>
2022-06-01 20:24:28 +0300

    add the router and all the fuction in product

--- backend/routes/permission.js
+++ backend/routes/permission.js
@@ -3,4 +3,4 @@ const express = require("express");
 const permissionRouter = express.Router();
 const createPermission = require("../controllers/permission")
 permissionRouter.post("/:id" ,createPermission )
-module.exports = permissionRouter
+module.exports = permissionRouter;

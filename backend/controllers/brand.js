7e0600f1350e7199e2eafe96b45b26cfa4e4ef4b
jehadalhorany <alhoranyjehad@gmail.com>
2022-06-02 23:01:43 +0300

    getbeandbyid

--- backend/controllers/brand.js
+++ backend/controllers/brand.js
@@ -40,10 +40,36 @@ const getAllBrand = (req,res)=>{
     });
 }; 
 
+const getBrandById =(req,res)=>{
+    const id =req.params.id;
+    const query =`SELECT * FROM brand WHERE id = ? `;
+    const data =[id];
+    connection.query(query,data,(err,result)=>{
+        if(err){
+            res.status(500).json({
+                success:false,
+                massage:"Server Error",
+                err:err,
+            });
+        }
+        if(!result.length){
+            res.status(404).json({
+                success:false,
+                massage:"The brand is Not found",
+
+            });
+        }
+        res.status(200).json({
+            success:true,
+            massage:`the brand ${id}`,
+            result:result,
+        });
+    });
+};
 
 
 
 
 
 module.exports={addBrand,
-    getAllBrand}
+    getAllBrand,getBrandById}

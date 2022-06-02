f2229967423af913c045cbcd44012d711db56db9
jehadalhorany <alhoranyjehad@gmail.com>
2022-06-02 22:50:14 +0300

    getAllBrand

--- backend/controllers/brand.js
+++ backend/controllers/brand.js
@@ -22,9 +22,28 @@ connection.query(query,data,(err,result)=>{
 };
 
 
+const getAllBrand = (req,res)=>{
+    const query = `SELECT * FROM brand WHERE is_deleted=0 ;`;
+    connection.query(query, (err, result) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                massage: "server error",
+                err: err,
+            });
+        }
+        res.status(200).json({
+            success: true,
+            massage: "All the brand",
+            result: result,
+        });
+    });
+}; 
 
 
 
 
 
-module.exports={addBrand}
+
+module.exports={addBrand,
+    getAllBrand}

20a9d15ba2c1c56c3352d85d98460f38da086448
jehadalhorany <alhoranyjehad@gmail.com>
2022-06-03 20:13:03 +0300

    add all categeory

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const CategoryRouter = express.Router();
-const { Createcategory, deleteCategoryById } = require("../controllers/category")
+const { Createcategory, deleteCategoryById,getAllCategory } = require("../controllers/category")
 const {Createsub_category}=require("../controllers/sub_category")
 CategoryRouter.delete("/:id", deleteCategoryById)
 CategoryRouter.post("/", Createcategory)
 CategoryRouter.post("/sub",Createsub_category)
+CategoryRouter.get("/",getAllCategory)
+
+
 module.exports = CategoryRouter;

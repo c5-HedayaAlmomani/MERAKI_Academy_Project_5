58f1332626485b96a0d6ef0b7ad99b0e4e1e1d78
HedayaAlmomani <almomani.hedaya@gmail.com>
2022-06-07 14:03:36 +0300

    edit

--- backend/routes/rate.js
+++ backend/routes/rate.js
@@ -1,10 +1,8 @@
-const express = require("express")
+const express = require("express");
 const rateRouter = express.Router();
-const {addRate ,getRate , updateRate , deleteRate ,getuserId } = require("../controllers/rate")
-const authentication = require("../middlewares/authentication")
-rateRouter.post("/",authentication , addRate)
-rateRouter.get("/:product_id" , getRate)
-rateRouter.put ("/" ,authentication, updateRate)
-rateRouter.delete("/:id", deleteRate)
-rateRouter.get("/user_id" , getuserId)
-module.exports = rateRouter ;
+const { addRate, getRate, deleteRate } = require("../controllers/rate");
+const authentication = require("../middlewares/authentication");
+rateRouter.post("/", authentication, addRate);
+rateRouter.get("/:product_id", getRate);
+rateRouter.delete("/:id", deleteRate);
+module.exports = rateRouter;

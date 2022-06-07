b40c2f9f2d8ae5af8260601d4e0704fafc304f9f
HedayaAlmomani <almomani.hedaya@gmail.com>
2022-06-07 23:08:54 +0300

    edit2

--- frontend/src/components/ProCB/index.js
+++ frontend/src/components/ProCB/index.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 
 const ProCB = () => {
+  const { token, isLoggedIn } = useSelector((state) => {
+   
+
+    return {
+      token: state.auth.token,
+      isLoggedIn: state.auth.isLoggedIn,
+    };
+  });
   const navigate = useNavigate();
   const { brand, category } = useParams();
   const [allPCat, setPAllCat] = useState([]);
@@ -21,21 +30,67 @@ const ProCB = () => {
         console.log(err);
       });
   };
+  const addToCart = async (id) => {
+    if (!token) return alert("Please login to continue buying");
+    await axios
+      .post(
+        `http://localhost:5000/cart`,
+        {
+          productId: id,
+          quantity: 1,
+        },
+        { headers: { authorization: `Bearer ${token}` } }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(token);
+        console.log(err);
+      });
+  };
 
   useEffect(func, []);
 
   return (
-    <div>
+    <div className="products">
       {allPCat.length &&
         allPCat.map((element, index) => {
-          return (
-            <div key={index} className="categoryCont">
-              <p>{element.title}</p>
-              <img onClick={()=>{
-                navigate(`/product/${element.id}`)
-              }} src={`${element.image}`} />
+          return (<div key={index} className="one_product">
 
+            <img src={`${element.image}`}
+                  onClick={() => {
+                    navigate(`/product/${element.id}`);
+                    console.log({allPCat});
+                  }}
+                   />
+          <div className="information">
+            <div>
+              {" "}
+              {element.title}
+              {element.price}
             </div>
+            <button
+              className="add_to_cart"
+              onClick={() => {
+                console.log(element.id);
+                addToCart(element.id);
+              }}
+            >
+              Add to Cart
+            </button>
+          </div>
+        </div>
+
+            // <div key={index} className="categoryCont">
+            //   <p>{element.title}</p>
+            //   <img
+            //     onClick={() => {
+            //       navigate(`/product/${element.id}`);
+            //     }}
+            //     src={`${element.image}`}
+            //   />
+            // </div>
           );
         })}
     </div>

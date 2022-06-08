a801bfea4b5814a397edc71a56e40402fa0b8e2b
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 <baha.abuellwan@gmail.com>
2022-06-08 13:56:32 +0300

    edit

--- frontend/src/components/productAdmin/index.js
+++ frontend/src/components/productAdmin/index.js
@@ -11,14 +11,13 @@ import { useEffect } from "react";
 import { useState } from "react";
 import("./style.css");
 
-
 const ProductAdmin = () => {
-//   const [title, setTitle] = useState("");
-//   const [description, setDescription] = useState("");
-//   const [price, setPrice] = useState("");
-//   const [image, setImage] = useState("");
-//   const [category_id, setCategory_id] = useState("");
-//   const [sub_category_id, setSub_Category_id] = useState("");
+  //   const [title, setTitle] = useState("");
+  //   const [description, setDescription] = useState("");
+  //   const [price, setPrice] = useState("");
+  //   const [image, setImage] = useState("");
+  //   const [category_id, setCategory_id] = useState("");
+  //   const [sub_category_id, setSub_Category_id] = useState("");
 
   //! redux =========
   const dispatch = useDispatch();
@@ -34,10 +33,6 @@ const ProductAdmin = () => {
 
   //! redux =========
 
-  useEffect(() => {
-    getAllProduct();
-  }, []);
-
   const getAllProduct = () => {
     axios
       .get(`http://localhost:5000/products`, {
@@ -45,6 +40,7 @@ const ProductAdmin = () => {
       })
       .then((result) => {
         dispatch(setProductsAction(result.data.result));
+        console.log(result);
       })
       .catch((err) => {
         console.log(err);
@@ -64,18 +60,20 @@ const ProductAdmin = () => {
         console.log(err);
       });
   };
-
+  useEffect(() => {
+    getAllProduct();
+  }, []);
 
   return (
     <div className="ProductAdmin">
       <table id="productT">
         <tr>
-          <th >#</th>
-          <th >Product Name</th>
-          <th >Description </th>
-          <th >Price </th>
-          <th >Brand </th>
-          <th >Category </th>
+          <th>#</th>
+          <th>Product Name</th>
+          <th>Description </th>
+          <th>Price </th>
+          <th>Brand </th>
+          <th>Category </th>
 
           <th /* style={{borderRight:"1px solid black"}} */>Actions</th>
         </tr>
@@ -83,19 +81,13 @@ const ProductAdmin = () => {
           products.map((element, index) => {
             return (
               <tr key={index}>
-                <td >{index + 1}</td>
-                <td >
-                  {element.description}
-                </td>{" "}
-                <td >{element.title}</td>
-                <td >{element.price}</td>
-                <td >
-                  {element.brand}
-                </td>
-                <td >
-                  {element.category}
-                </td>
-                <td >
+                <td>{index + 1}</td>
+                <td>{element.description}</td>
+                <td>{element.title}</td>
+                <td>{element.price}</td>
+                <td>{element.brand}</td>
+                <td>{element.category}</td>
+                <td>
                   <button
                     className="delete_button"
                     onClick={() => {
@@ -105,18 +97,28 @@ const ProductAdmin = () => {
                   >
                     Delete
                   </button>
-                  <button className="edit_button" onClick={()=>{navigate(`/Admin/product/edit/${element.id}`)
-                
-                }}>Edit</button>
+                  <button
+                    className="edit_button"
+                    onClick={() => {
+                      navigate(`/Admin/product/edit/${element.id}`);
+                    }}
+                  >
+                    Edit
+                  </button>
                 </td>
               </tr>
             );
           })}
       </table>
       <div className="Add_Product">
-        <button className="Add_Product" onClick={()=>{navigate("/Admin/product/create")
-                
-              }}>Add Product</button>
+        <button
+          className="Add_Product"
+          onClick={() => {
+            navigate("/Admin/product/create");
+          }}
+        >
+          Add Product
+        </button>
       </div>
     </div>
   );

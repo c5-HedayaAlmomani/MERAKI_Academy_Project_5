e154336df1ac3e22b4c2b8cf587869e9d6c09dab
jehadalhorany <alhoranyjehad@gmail.com>
2022-06-15 00:44:24 +0300

    add function and test it

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const CategoryRouter = express.Router();
 const { Createcategory, deleteCategoryById,getAllCategory } = require("../controllers/category")
+const {getAllCategoryPagination}=require("../controllers/paginationProduct")
 const {Createsub_category}=require("../controllers/sub_category")
 CategoryRouter.delete("/:id", deleteCategoryById)
 CategoryRouter.post("/", Createcategory)
 CategoryRouter.post("/sub",Createsub_category)
 CategoryRouter.get("/",getAllCategory)
+
+CategoryRouter.get("/pagination/:page", getAllCategoryPagination);
+
+
+
 
 
 module.exports = CategoryRouter;

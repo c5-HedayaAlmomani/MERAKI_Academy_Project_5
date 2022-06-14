2b3fc616974bffd5d19ef992b7df7570f2f7341c
HedayaAlmomani <almomani.hedaya@gmail.com>
2022-06-14 15:12:53 +0300

    pagination backend

--- backend/controllers/paginationProduct.js
+++ backend/controllers/paginationProduct.js
@@ -24,5 +24,29 @@ const getAllProductPagination = (req, res) => {
     });
   });
 };
+const getfourProductPagination = (req, res) => {
+  const limit = 4;
 
-module.exports = getAllProductPagination;
+  const page = req.params.page;
+
+  const offset = (page - 1) * limit;
+
+  const query = `SELECT * FROM Products WHERE is_deleted=0 LIMIT ${limit} OFFSET ${offset};`;
+
+  connection.query(query, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "server error",
+        err: err,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      massage: "All the Product",
+      result: result,
+    });
+  });
+};
+
+module.exports = {getAllProductPagination , getfourProductPagination};

3aef7ecc182a2d83c490d15d03e7e8cdb0daf9ff
jehadalhorany <alhoranyjehad@gmail.com>
2022-06-15 00:43:38 +0300

    add function and anther style

--- backend/controllers/paginationProduct.js
+++ backend/controllers/paginationProduct.js
@@ -49,4 +49,31 @@ const getfourProductPagination = (req, res) => {
   });
 };
 
-module.exports = {getAllProductPagination , getfourProductPagination};
+
+
+const getAllCategoryPagination = (req, res) => {
+  const limit = 6;
+
+  const page = req.params.page;
+
+  const offset = (page - 1) * limit;
+
+  const query = `SELECT * FROM category WHERE is_deleted=0 LIMIT ${limit} OFFSET ${offset};`;
+
+  connection.query(query, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "server error",
+        err: err,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      massage: "All the category",
+      result: result,
+    });
+  });
+};
+
+module.exports = {getAllProductPagination , getfourProductPagination,getAllCategoryPagination};
